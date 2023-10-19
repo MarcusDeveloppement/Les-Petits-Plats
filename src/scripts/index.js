231e402f8a1e47c08feb51837bfbdfe8e397bd5f
MarcusDeveloppement <marcus.frontdev@gmail.com>
2023-10-19 21:20:34 +0200

    revised code, need to add reduce, filter and map for the comparison

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,12 @@
 export function filterTagsSelected() {
-  //elements select
-  const tags = document.querySelectorAll(".custom-li");
-  const removeBtn = document.querySelectorAll(".remove-tag-button");
-  const elements = document.querySelectorAll(".global");
+  // select elements
+  const tags = Array.from(document.querySelectorAll(".custom-li"));
+  const removeBtn = Array.from(document.querySelectorAll(".remove-tag-button"));
+  const elements = Array.from(document.querySelectorAll(".global"));
+
   //listener for the remove buttons that's delete the classs selectedList
   removeBtn.forEach((btn) => {
-    btn.addEventListener("click", function () {
+    btn.addEventListener("click", () => {
       tags.forEach((tag) => {
         if (tag.classList.contains("selectedList")) {
           tag.classList.remove("selectedList");
@@ -15,7 +16,7 @@ export function filterTagsSelected() {
     });
   });
 
-  //Browse the list and make an event
+  //tags listener
   tags.forEach((tag) => {
     tag.addEventListener("click", (event) => {
       const search = event.target.textContent.toLowerCase();
@@ -26,29 +27,22 @@ export function filterTagsSelected() {
 
   function toggleSelection(tag) {
     const tagListClass = tag.classList.contains("selectedList");
-
     if (!tagListClass) {
       tag.classList.add("selectedList");
     }
   }
 
-  //up date the display based on tags
+  // updating the displays according to tags
   function updateFilter() {
     elements.forEach((elem) => {
       const elemText = elem.textContent.toLowerCase();
-      let isVisible = true;
-
-      tags.forEach((tag) => {
-        if (tag.classList.contains("selectedList")) {
-          const tagText = tag.textContent.toLowerCase();
-          // regular expression for search the entire word
+      const isVisible = tags
+        .filter((tag) => tag.classList.contains("selectedList"))
+        .map((tag) => tag.textContent.toLowerCase())
+        .reduce((isVisible, tagText) => {
           const wordRegExp = new RegExp("\\b" + tagText + "\\b", "i");
-
-          if (!wordRegExp.test(elemText)) {
-            isVisible = false;
-          }
-        }
-      });
+          return isVisible && wordRegExp.test(elemText);
+        }, true);
 
       elem.style.display = isVisible ? "block" : "none";
     });
